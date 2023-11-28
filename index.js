9f9473c87e25686d83f53a63754379742893fd52
Jonathan Kulak <lackadaize@msn.com>
2023-11-28 05:46:14 -0500

    This is the git commit for the Arithmetic Lab assignment.

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+// multiply
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+// random
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+console.log(random);
+
+// mod
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod);
+
+// max
+
+let max = Math.max(4, 8, 12, 16, 20);
+
+console.log(max);
